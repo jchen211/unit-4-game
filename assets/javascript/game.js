1d90866e7b30e7495a6945bad7f1e4ad108a6264
Jessica Chen <jchen@jchen.attlocal.net>
2018-12-16 22:54:01 -0600

    all crystals click and adds to total

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -32,35 +32,54 @@ var imgCrys1 = $('#crystal-1');
     imgCrys1.attr('data-crys', crysNum.getCrystal()); 
     $('crystal-1').append(imgCrys1);
 
-var imgCrys1 = $('#crystal-2');
-    imgCrys1.attr('data-crys', crysNum.getCrystal()); 
-    $('crystal-2').append(imgCrys1);
+var imgCrys2 = $('#crystal-2');
+    imgCrys2.attr('data-crys', crysNum.getCrystal()); 
+    $('crystal-2').append(imgCrys2);
     
-var imgCrys1 = $('#crystal-3');
-    imgCrys1.attr('data-crys', crysNum.getCrystal()); 
-    $('crystal-3').append(imgCrys1);
+var imgCrys3 = $('#crystal-3');
+    imgCrys3.attr('data-crys', crysNum.getCrystal()); 
+    $('crystal-3').append(imgCrys3);
 
-var imgCrys1 = $('#crystal-4');
-    imgCrys1.attr('data-crys', crysNum.getCrystal()); 
-    $('crystal-4').append(imgCrys1);
+var imgCrys4 = $('#crystal-4');
+    imgCrys4.attr('data-crys', crysNum.getCrystal()); 
+    $('crystal-4').append(imgCrys4);
 
-$('#crystal-1').on("click", function() {
-    var crystalValue = ($(this).attr("data-crys"));
-    crystalValue = parseInt(crystalValue);
-
-    var newTotal = total += crystalValue;
-    $('#addedTotal').text(newTotal);
-    })
+function crysClicks () {
 
     $('#crystal-1').on("click", function() {
+        var crystalValue = ($(this).attr("data-crys"));
+        crystalValue = parseInt(crystalValue);
+
+        var newTotal = total += crystalValue;
+        $('#addedTotal').text(newTotal);
+    })
+
+    $('#crystal-2').on("click", function() {
+        var crystalValue = ($(this).attr("data-crys"));
+        crystalValue = parseInt(crystalValue);
+    
+         var newTotal = total += crystalValue;
+        $('#addedTotal').text(newTotal);
+        })    
+
+    $('#crystal-3').on("click", function() {
         var crystalValue = ($(this).attr("data-crys"));
         crystalValue = parseInt(crystalValue);
     
         var newTotal = total += crystalValue;
         $('#addedTotal').text(newTotal);
-        })    
+        })  
 
+    $('#crystal-4').on("click", function() {
+        var crystalValue = ($(this).attr("data-crys"));
+        crystalValue = parseInt(crystalValue);
+    
+        var newTotal = total += crystalValue;
+        $('#addedTotal').text(newTotal);
+        })  
+    }
 
+    crysClicks();
 
     startGame();
 
